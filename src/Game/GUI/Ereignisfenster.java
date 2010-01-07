bda6607fb40e1d7d754c8da23d73c72f1e6a5f57
mail.mrgoro <mail.mrgoro@d361eb40-eb14-11de-88d6-afc75ad17f39>
2010-01-07 10:56:18 +0000

    Charakterauswahl und Spieloberfläche angepasst!

--- src/Game/GUI/Ereignisfenster.java
+++ src/Game/GUI/Ereignisfenster.java
@@ -2,14 +2,12 @@ package Game.GUI;
 
 import javax.swing.JDialog;
 import Game.Ereignis;
-import Game.SpielAnwendung;
 
 public class Ereignisfenster extends JDialog {
 
 	private static final long serialVersionUID = 8375152556875337533L;
 	
 	public Ereignisfenster(Ereignis er) {
-		super();
 		this.setLocationRelativeTo(null);
 		this.setSize(400, 200);
 		this.setVisible(true);
