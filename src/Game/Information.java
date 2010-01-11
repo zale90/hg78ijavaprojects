b1b6c626e231d76a4944a1cb7eac00c8c69a9bd5
mail.mrgoro <mail.mrgoro@d361eb40-eb14-11de-88d6-afc75ad17f39>
2010-01-11 15:31:01 +0000

    Öffentliche statische Variablen für die ersten beiden zu übergebenen Werte eingefügt.

--- src/Game/Information.java
+++ src/Game/Information.java
@@ -13,6 +13,25 @@ public class Information {
 	private int änderungsart;
 	private int wert;
 	
+	public static final int AENDERN_NAHRUNG = 1;
+	public static final int AENDERN_NAHRUNGS_FAKTOR = 2;
+	public static final int AENDERN_GESUNDHEIT = 3;
+	public static final int AENDERN_GESUNDHEITS_FAKTOR = 4;
+	public static final int AENDERN_SOZIALES = 5;
+	public static final int AENDERN_SOZIALES_FAKTOR = 6;
+	public static final int AENDERN_LUXUS = 7;
+	public static final int AENDERN_LUXUS_FAKTOR = 8;
+	public static final int AENDERN_KINDER = 9;
+	public static final int AENDERN_KINDER_FAKTOR = 10;
+	public static final int AENDERN_ZEIT = 11;
+	public static final int AENDERN_ZEIT_PRO_RUNDE = 12;
+	public static final int AENDERN_GELD = 13;
+	public static final int AENDERN_GELD_PRO_MONAT = 14;
+	
+	public static final int ART_AUF_WERT = 1;
+	public static final int ART_UM_WERT = 2;
+	public static final int ART_AUF_PROZENTWERT = 3;
+	public static final int ART_UM_PROZENTWERT = 4;
 	
 	/**
 	 * @param zuÄndern 1=Nahrung, 2=N.faktor, 3=Gesundheit, 4=G.Faktor, 
