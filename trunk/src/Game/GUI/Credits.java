017bdd2be06ecb3a28068a65cf8a4a6f5508a980
mail.mrgoro <mail.mrgoro@d361eb40-eb14-11de-88d6-afc75ad17f39>
2010-01-13 16:56:42 +0000

    TextPane für die Credits. Sehr wenig .. nur für Henrik!

--- trunk/src/Game/GUI/Credits.java
+++ trunk/src/Game/GUI/Credits.java
@@ -1,14 +1,20 @@
 package Game.GUI;
 
 import java.awt.Color;
-
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 import javax.swing.border.LineBorder;
 
 public class Credits extends JPanel {
 	
 	private static final long serialVersionUID = 2500415931767014676L;
-
+	
+	private final String text = "" +
+			"Hier findet ihr die Cedits!\n" +
+			"";
+	
+	private JTextPane tpInfos;
+	
 	public Credits() {
 		this.setSize(494, 672);
 		this.setLocation(500, 0);
@@ -16,6 +22,16 @@ public class Credits extends JPanel {
 		this.setBackground(Color.BLACK);
 		
 		this.setBorder(new LineBorder(Color.BLACK, 2));
+		
+		// Textfeld anzeigen
+		tpInfos = new JTextPane();
+		tpInfos.setText(text);
+		tpInfos.setSize(490, 670);
+		tpInfos.setLocation(0, 0);
+		tpInfos.setBackground(Color.BLACK);
+		tpInfos.setForeground(Color.WHITE);
+		this.add(tpInfos);
+		
 		
 		this.setVisible(true);
 	}
